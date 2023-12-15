e7868c5081d358caae1e236d600e478427e4f101
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2023-12-15 14:04:59 -0500

    checkout

--- seleniumDec/src/test/java/com/seleniumDec/eCommerceWeb.java
+++ seleniumDec/src/test/java/com/seleniumDec/eCommerceWeb.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -17,11 +18,19 @@ public class eCommerceWeb extends browserCl {
 	String search_text = "tank";
 	String product_name = "Nora Practice Tank";
 	String quantity_no = "2";
+	String address = "123 abc";
+	String city = "salem";
+	String country_value = "37";
+	String zip = "12345";
+	String phone = "1234567890";
 	By size = By.xpath("//div[@class='swatch-option text' and @index='2']");
 	By color = By.xpath("//div[@class='swatch-option color' and @index='1']");
-	int cart_price_in, cart_qty_in, cart_sub , cart_subtotal_in;
-	String ccart_qty, ccart_price,  ccart_subtotal,  ex_size , ex_color, cccart_price,  vval,vvalu,cccart_subtotal, del_text ;
+	int cart_price_in, cart_qty_in, cart_sub , cart_subtotal_in,cart_check_subtotal_in,ship_total_amount_in;
+	String ccart_qty, ccart_price,  ccart_subtotal,  ex_size , ex_color, cccart_price,  vval,vvalu,cccart_subtotal, del_text,cccart_check_subtotal,ccart_check_subtotal,vvalue ,ship_total_full, vvalueShip,ship_total_amount;
+	String actu_placed_text,act_placed_order;
 	String exp_del_text = "You have no items in your shopping cart.";
+	String exp_placed_text = "Thank you for your purchase!";
+	
 	By signin = By.xpath("//div[@class='panel header']//a[contains(text(),'Sign In')]");
 	By email = By.id("email");
 	By pass = By.id("pass");
@@ -45,10 +54,29 @@ public class eCommerceWeb extends browserCl {
 	By cart_qty = By.xpath("//table[@id='shopping-cart-table']//span[@class='label']/following-sibling::input");
 	By cart_price = By.xpath("//table[@id='shopping-cart-table']//td[@class='col price']//span[@class='price']");
 	By cart_subtotal = By.xpath("//table[@id='shopping-cart-table']//td[@class='col subtotal']//span[@class='price']");
+	By cart_checkout_total = By.xpath("//span[@data-bind='text: getValue()'][normalize-space()='$78.00']");
+	//span[@data-bind='text: getValue()'][normalize-space()='$78.00']
 	//delete item cart page
 	By cart_del = By.xpath("//div[@class='actions-toolbar']//a[@title='Remove item']");
 	By cart_del_text = By.xpath("//div[@class='cart-empty']/p[contains(text(),'no')]");
-	
+	//checkout 
+	By check_Btn = By.xpath("//li[@class='item']/button[@title='Proceed to Checkout']");
+	//shipping
+	By ship_addr = By.xpath("//input[@id='G7JIJSA']");
+	By ship_city = By.xpath("//input[@id='X7I9QRU']");
+	By ship_country = By.xpath("//select[@id='ULXYDSP']");
+	By ship_zip = By.xpath("//input[@id='LOVOU3V']");
+	By ship_phone = By.xpath("//input[@id='L8IVCQT']");
+	By ship_method = By.xpath(" //input[@name='ko_unique_1']");
+	By ship_next = By.xpath("//span[normalize-space()='Next']");
+	By ship_total = By.xpath("//tr[@class='grand totals']//span[@class='price']");
+	By ship_place = By.xpath("//button[@title='Place Order']");
+	//after placing order
+	By placed_text = By.xpath("//h1[@class='page-title']/span");
+	By placed_order = By.xpath("//a[@class='order-number']/strong");
+	By ship_already = By.xpath("//div[@class='shipping-address-items']");
+
+			
 @Test(priority=0)
 	public void Login() throws Exception {
 	/*
@@ -164,10 +192,19 @@ for(int i=0; i<lin.length; i++) {
 cccart_subtotal = vvalu.substring(1);
 		 cart_subtotal_in = Integer.parseInt(cccart_subtotal);
 	Assert.assertEquals(cart_subtotal_in, cart_sub);	
+	
+	ccart_check_subtotal = driver.findElement(cart_checkout_total).getText();
+	String[] lind1 = ccart_check_subtotal.split("\\.");
+for(int i=0; i<lind1.length; i++) {
+	 vvalue = lind1[0];
+}
+cccart_check_subtotal = vvalue.substring(1);
+		 cart_check_subtotal_in = Integer.parseInt(cccart_check_subtotal);
+	Assert.assertEquals(cart_check_subtotal_in, cart_subtotal_in);
 }
 
 
-@Test(priority=4, dependsOnMethods = "subtotal")
+
 public void deleteCart() {
 /*
 Scenario 4: Delete the cart items
@@ -181,14 +218,53 @@ driver.findElement(cart_del).click();
 }
 
 
+@Test(priority=4, dependsOnMethods = "subtotal")
+public void checkout() throws Exception {
 	/*
 Scenario 5: Complete and Verify Checkout
 Continue Checkout and enter checkout details.
 Click the Complete Checkout button.
 Verify Successful Checkout Button.
 	 */
+ driver.findElement(check_Btn).click();
+ Thread.sleep(5000);
+ 
+ if((driver.findElement(ship_already).isDisplayed())==false) {
+	
+ driver.findElement(ship_addr).sendKeys(address);
+ driver.findElement(ship_city).sendKeys(city);
+ 
+ Select s = new Select(driver.findElement(ship_country));
+ s.selectByValue(country_value);
+ 
+  driver.findElement(ship_zip).sendKeys(zip);
+ driver.findElement(ship_phone).sendKeys(phone);
+ 
+ }
+	driver.findElement(ship_method).isSelected();
+ 
+ 
+ driver.findElement(ship_next).click();
+ Thread.sleep(3000);
+ 
+ship_total_full = driver.findElement(ship_total).getText();
+ 	String[] lind2 = ship_total_full.split("\\.");
+for(int i=0; i<lind2.length; i++) {
+	 vvalueShip = lind2[0];
+}
+ship_total_amount = vvalueShip.substring(1);
+		 ship_total_amount_in = Integer.parseInt(ship_total_amount);
+	Assert.assertEquals(ship_total_amount_in, cart_subtotal_in);
 
-
+ driver.findElement(ship_place).click();
+ Thread.sleep(3000);
+actu_placed_text =  driver.findElement(placed_text).getText();
+ Assert.assertEquals(actu_placed_text, exp_placed_text);
+ 
+ act_placed_order = driver.findElement(placed_order).getText();
+ System.out.println("Your order number is: " +act_placed_order);
+ 
+}
 
 
 
