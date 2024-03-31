00acdbb92c0dfc582994a7bf9744764878689ae7
Mathumathi Balakrishnan <mathumathibalakrishnan@DheenatalansAir.fios-router.home>
2024-03-30 22:59:58 -0400

    prime

--- seleniumDec/src/test/java/CodingProbMarch/Mar29.java
+++ seleniumDec/src/test/java/CodingProbMarch/Mar29.java
@@ -1,9 +1,36 @@
 package CodingProbMarch;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Mar29 {
 
 	
 	public static void largestPrimeNo() {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number: ");
+		int num = sc.nextInt();
+		List ar = new ArrayList<>();
+		for(int i=2; i<=num; i++) {
+			boolean isPrime = true;
+			for(int j=2; j<i; j++) {
+		if(i%j==0) {
+		
+			isPrime = false;
+			break;
+		}
+			}
+			if(isPrime) {
+			ar.add(i);
+			
+			}
+		}
+		
+		Collections.reverse(ar);
+		System.out.println(	ar.get(0));	
 		
 	}
 	
@@ -11,7 +38,24 @@ public class Mar29 {
 	
 	
 	
-	
+	public static void isPrimeNo(int num) {
+		
+		for(int i=2; i<=num; i++) {
+			boolean isPrime = true;
+			for(int j=2; j<i; j++) {
+				if(i%j==0) {
+					isPrime=false;
+					break;
+				}
+				
+			}
+			if(isPrime) {
+				System.out.println(i);
+			}
+		}
+		
+		
+	}
 	
 	
 	public static void main(String[] args) {
@@ -20,9 +64,9 @@ public class Mar29 {
 		//a) Make sure the prime number is less than or equal to a given number.
 		//b) Ask for user input
 		
+		largestPrimeNo();
 		
-		
-		
+		isPrimeNo(100);
 	}
 
 }
