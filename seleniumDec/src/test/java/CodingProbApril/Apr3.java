a6c939be6c0bd8313ed48339bfeff56cb7e5594a
Mathumathi Balakrishnan <mathumathibalakrishnan@DheenatalansAir.fios-router.home>
2024-04-02 23:08:14 -0400

    git created

---
+++ seleniumDec/src/test/java/CodingProbApril/Apr3.java
@@ -0,0 +1,26 @@
+package CodingProbApril;
+
+public class Apr3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+//Code 1: Sum of two. Write a method that accepts an int[] array and an int number, and find 2 elements in the array that sum is equal to the given int. Assume that an input array will have only one pair of numbers that sum equal to our given number. It will always have this pair. See input and output examples. I use the Brute Force algorithm.
+
+//Code 2: The Fibonacci. It is a series of numbers where the next number is the sum of the previous two numbers. The first two numbers of the Fibonacci is 0 followed by 1. Write a method that will accept one int number n. The method will print n number of Fibonacci numbers.
+
+//Code 3: 19. Balanced String. This question has become very popular lately. The program should find out if an input string is balanced or not.
+		/*
+		 System.out.println(isBalanced("[{()}]")); // true
+        System.out.println(isBalanced("[({(})]")); // false
+        System.out.println(isBalanced("{[}")); // false
+        System.out.println(isBalanced("({}{}([{}]))")); // true
+        System.out.println(isBalanced("({")); // false
+        
+		 */
+//Code 4: Sort array without built-in sort methods. In this example, we will use selection sort and bubble sort algorithms to sort our array.
+	
+	}
+
+}
