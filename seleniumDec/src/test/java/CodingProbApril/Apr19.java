7c247f9c5d78947e974b67ab425f99eb59b42330
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2024-04-19 07:22:51 -0400

    dsfs

---
+++ seleniumDec/src/test/java/CodingProbApril/Apr19.java
@@ -0,0 +1,20 @@
+package CodingProbApril;
+
+public class Apr19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+How do you print the first non-repeated character from a string? (solution)
+How do you convert a given String into int like the atoi()? (solution)
+How do you reverse words in a given sentence without using any library method? (solution)
+How do you check if two strings are a rotation of each other? (solution)
+How do you find the length of the longest substring without repeating characters? (solution)
+Given string str, How do you find the longest palindromic substring in str? (solution)
+How to convert a byte array to String? (solution)
+how to remove the duplicate character from String? (solution)
+How to find the maximum occurring character in given String? (solution)
+*/
+	}
+
+}
